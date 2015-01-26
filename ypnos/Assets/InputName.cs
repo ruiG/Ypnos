4e0492cb64510dfa6a989f854799a4718b47dee6
GuilhermeSCP <luisgui2007@gmail.com>
2015-01-26 21:12:18 +0000

    Now it possible to define the player's name and proceed to game

---
+++ ypnos/Assets/InputName.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputName : MonoBehaviour {
+
+
+	public UnityEngine.UI.InputField inputField;
+	public string name;
+	public UnityEngine.UI.Text text;
+
+
+	// Update is called once per frame
+	void Update () {
+		name = inputField.text;
+
+		if(inputField.isFocused && inputField.text != "" && Input.GetKey(KeyCode.Return)) {
+			//store name forever
+			Application.LoadLevel("Level");
+		}
+	}
+}
